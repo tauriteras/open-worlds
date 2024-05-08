7cf2e3485b8b13b47ced052916af7fda66106707
Tauri Teras <57317414+tauriteras@users.noreply.github.com>
2024-05-08 08:48:19 +0300

    Fresh Start

---
+++ SERVER/server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import path from "path";
+import http from "http";
+
+import * as fs from 'fs';
+
+import { Server } from "socket.io";
+import { on } from "events";
+
+const port = 3069;
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:3069", "http://localhost:5173"]
+    },
+});
+
+app.use(express.static("dist"));
+
+const indexPath = path.join(process.cwd(), "dist", "index.html");
+//const indexPath = path.join("../BUILD", "dist", "index.html");
+
+
+app.get("*", (req, res) => {
+    res.sendFile(indexPath);
+
+    req.on("error", (err) => {
+        console.error(err);
+        res.statusCode = 400;
+        res.end();
+    });
+
+});
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+
+});
+
+
